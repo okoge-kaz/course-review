efcfacfb2582305aae73d65e7b40bed558785a50
kazuki <kaz.tokyo.tech20@gmail.com>
2021-08-02 21:20:13 +0900

    CONTACT USまわり

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from 'react'
 import { Container } from 'react-bootstrap'
-import styles from './Footer.module.css'
+import styles from './Footer.module.scss'
+import Link from 'next/link'
+
+const link =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfIW7Dz5UWB4wb75k5zgJk46Y_2bmVm2EUUYTZBOOPBHhleXA/viewform'
 
 const Footer: FC = () => (
   <div className={styles.main}>
-    <div className={styles.text}>CONTACT US</div>
+    <div className={styles.text}>
+      <Link href={link}>
+        <a className={styles.link}>CONTACT US</a>
+      </Link>
+    </div>
     <div className={styles.text}>Copyright 2021 Titech Info</div>
   </div>
 )
