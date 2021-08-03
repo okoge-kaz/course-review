619434d87f491ae3d7b3b95ab466040953ee12f0
kazuki <kaz.tokyo.tech20@gmail.com>
2021-08-03 18:39:01 +0900

    logo link

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react'
+import Link from 'next/link'
 import { Container } from 'react-bootstrap'
 import styles from './Header.module.scss'
 
 const Header: FC = () => (
   <Container>
-    <div className={styles.main}>Titech Info</div>
+    <div className={styles.main}>
+      <Link href="/">
+        <a className={styles.link}>Titech Info</a>
+      </Link>
+    </div>
   </Container>
 )
 
