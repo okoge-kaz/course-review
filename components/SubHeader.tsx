2cf3e973f349e2fb14ff963ba811f830974eb091
kazuki <kaz.tokyo.tech20@gmail.com>
2021-08-10 22:33:50 +0900

    metaタグ修正

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -7,7 +7,8 @@ const SubHeader: FC = () => (
     <Container className={styles.JumbotronSize}>
       <h1>逆評定</h1>
       <p>
-        説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと
+        皆さんから頂いた情報を元に東工大の講義について、課題や難易度などの情報をまとめています。
+        講義を選ぶ際などの参考にして下さい。
       </p>
     </Container>
   </Container>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,6 @@ class MyDocument extends Document {
       <Html lang="ja">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lobster" />
         <Head>
-        <meta charSet="utf-8"/>
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-        <meta property="og:title" content="逆評定 - Titech Info : 東工大情報サイト"/>
-        <meta property="og:image" content="https://course-review-tmp.titech.info/images/course-review-ogp.png/"/>
         </Head>
         <body>
           <Main />
