744859c153adce0cdddf37b07ca18b1043f231c4
kazuki <kaz.tokyo.tech20@gmail.com>
2021-08-16 00:54:35 +0900

    fix footer css

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -6,13 +6,15 @@ const link =
   'https://docs.google.com/forms/d/e/1FAIpQLSfIW7Dz5UWB4wb75k5zgJk46Y_2bmVm2EUUYTZBOOPBHhleXA/viewform'
 
 const Footer: FC = () => (
-  <div className={styles.main}>
-    <div className={styles.text}>
-      <Link href={link}>
-        <a className={styles.link}>CONTACT US</a>
-      </Link>
+  <div className={styles.Container}>
+    <div className={styles.main}>
+      <div className={styles.text}>
+        <Link href={link}>
+          <a className={styles.link}>CONTACT US</a>
+        </Link>
+      </div>
+      <div className={styles.text}>Copyright 2021 Titech Info</div>
     </div>
-    <div className={styles.text}>Copyright 2021 Titech Info</div>
   </div>
 )
 
