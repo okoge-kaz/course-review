f46875c0a327cabc67df44b98db8d2a5adab91f2
kazuki <kaz.tokyo.tech20@gmail.com>
2021-09-28 15:32:14 +0900

    add privacyPolicyLink

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,6 +3,7 @@ import styles from './Footer.module.scss'
 import Link from 'next/link'
 
 const link = 'https://twitter.com/tech_techfrom18'
+const privacyPolicyLink = 'https://titech.app/products/titechinfo/course_review/privacy_policy/'
 
 const Footer: FC = () => (
   <div className={styles.Container}>
@@ -10,6 +11,9 @@ const Footer: FC = () => (
       <div className={styles.text}>
         <Link href={link}>
           <a className={styles.link}>CONTACT US</a>
+        </Link>
+        <Link href={privacyPolicyLink}>
+          <a className={styles.link}>Privacy Policy</a>
         </Link>
       </div>
       <div className={styles.text}>Copyright 2021 Titech Info</div>
